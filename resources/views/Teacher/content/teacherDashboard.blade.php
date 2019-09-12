2ffeb0a9a6ae8922c84a9bd569404f1e74273b1c
kicinka@spse-po.sk <Kristian2016>
2019-09-12 22:37:35 +0200

    Pridanie otazok potreba opravit sidebar a pridat nazov a triedu testu

--- resources/views/Teacher/content/teacherDashboard.blade.php
+++ resources/views/Teacher/content/teacherDashboard.blade.php
@@ -13,7 +13,7 @@
         <div class="teacher-tests col-md-3 px-3 py-3 shadow p-3 mb-5 bg-white rounded text-left d-inline-block ml-4">
             <h3>Your tests:</h3>
             <p>count of tests :</p>
-            <button class="btn btn-danger float-lg-right mt-3">Make new Test</button>
+            <button class="btn btn-danger float-lg-right mt-3" onclick="window.location='{{ route('testCreate') }}'">Make new Test</button>
         </div>
         <div class="teacher-materials col-md-4 px-3 py-3 shadow p-3 mb-5 bg-white rounded text-left d-inline-block ml-4">
             <h3>Your uploaded Materials:</h3>
--- resources/views/Tests/create.blade.php
+++ resources/views/Tests/create.blade.php
@@ -3,14 +3,7 @@
 
 <?php
 use phpDocumentor\Reflection\Types\This;
-
- $index=1;
- $end=false;
- $options=2;
- function moreOptions($options){
-     $options++;
-     return view('Tests.create')->with('$options',$options);
- }
+use App\Http\Controllers\TestsController;
 
 ?>
 
@@ -27,58 +20,38 @@ use phpDocumentor\Reflection\Types\This;
                 <div class="row">
                     <div class="container px-2 py-2 mx-3 my-2 shadow p-3 mb-5 bg-white rounded text-left d-inline-block ">
                         <div class="label form-group col-md-6">
-                            <h3>Question {{ $index }}</h3>
+                            <h3>Question {{ isset($index) }}</h3>
                             <div class="question">
-                                {{ Form::text("question_$index",null,[
-                                    'placeholder'=>" Type Question $index ",
-                                    'class'=>'form-control',
-                                ]) }}
+                                {{ Form::text("question_$index",null,['placeholder'=>" Type Question $index",'class'=>'form-control', ]) }}
                             </div>
                             <div class="options py-4">
-                                @for($i=0;$i<$options;$i++)
+                                @for($i=0;$i<$answers;$i++)
                                     <div class="option py-2">
-                                        {{ Form::text("option_$i",null,[
-                                    'placeholder'=>"Option $i",
-                                    'class'=>'form-control',
-                                    ]) }}
+                                        {{ Form::text("answer_$i",null,['placeholder'=>"Answer $i",'class'=>'form-control',]) }}
+                                    </div>
+                                    <div class="checkbox float-left px-2 py-1">
+                                        {!! Form::label('correct_'.$i,'Correct',['class'=>'control-label']) !!}
+                                        {!! Form::hidden('correct_'.$i,0) !!}
+                                        {!! Form::checkbox('correct_'.$i,1,false,[]) !!}
                                     </div>
                                 @endfor
                             </div>
-                            <div class="handler py-4">
+                            <div class="handler py-2">
                                 <div class="form-group">
-                                    <div class="row">
+                                    <div class="row py-2 d-block">
                                         <div class="handler-buttons">
-                                            <div class="hanlder-button px-2 d-inline-block ml-2">
-                                                {!!  Form::button('Save',[
-                                            'class'=>'btn btn-primary',
-                                             'type'=>'submit',
-
-                                         ]) !!}
+                                            <div class="hanlder-button px-2 ml-2 py-2">
+                                                {!!  Form::button('Save',['class'=>'btn btn-success','type'=>'submit',]) !!}
                                             </div>
+                                        </div>
+                                    </div>
+                                    <div class="row py-2">
+                                        <div class="handler-button pt-4 px-2 d-inline-block float-left ml-2 ml-2">
+                                            {!!  Form::button('Back',['class'=>'btn btn-danger', ]) !!}
+                                        </div>
 
-                                            <div class="handler-button px-2 d-inline-block">
-                                                {!!  Form::button('+ Question',[
-                                                'class'=>'btn btn-secondary',
-
-                                             ]) !!}
-                                            </div>
-
-                                            <div class="handler-button px-2 d-inline-block">
-                                                {!!  Form::button('+ Option',[
-                                               'class'=>'btn btn-success',
-                                               'onclick'=>"function (){
-
-                                               }",
-
-                                            ]) !!}
-                                            </div>
-
-                                            <div class="handler-button px-2 d-inline-block">
-                                                {!!  Form::button('Back',[
-                                               'class'=>'btn btn-danger',
-
-                                            ]) !!}
-                                            </div>
+                                        <div class="handler-button pt-4 px-2 d-inline-block float-right ml-2">
+                                            {!!  Form::button('Next',['class'=>'btn btn-primary',]) !!}
                                         </div>
                                     </div>
                                 </div>
