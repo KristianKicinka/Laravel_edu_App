d13ff22fc12c5e5abf65e3ec123ec96f85293a36
KristianKicinka <kristian.kicinka@gmail.com>
2019-09-15 23:29:57 +0200

    Vytvorenie testu formulara frontend bez uloženia do DB. Pridanie vypisu studentov do jednoduchej tabuľky.

--- resources/views/Teacher/content/teacherStudents.blade.php
+++ resources/views/Teacher/content/teacherStudents.blade.php
@@ -20,14 +20,21 @@
                     <th>ID</th>
                     <th>Name</th>
                     <th>Email</th>
+                    <th>Actions</th>
                 </tr>
                 </thead>
+
                 <tbody>
                 @foreach($users as $user)
                     <tr>
                         <td>{{ $user->id }}</td>
                         <td>{{ $user->name }}</td>
                         <td>{{ $user->email }}</td>
+                        <td>
+                            <button class="btn btn-success">Edit</button>
+                            <button class="btn btn-danger">Delete</button>
+                        </td>
+
                     </tr>
                 @endforeach
                 </tbody>
