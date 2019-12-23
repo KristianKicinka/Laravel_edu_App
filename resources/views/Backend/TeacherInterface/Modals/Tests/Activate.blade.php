1b09e81ab77b61e1434b16c338d68c03ca1f02ce
KristianKicinka <kristian.kicinka@gmail.com>
2019-12-23 23:50:21 +0100

    After add new design to login and register page. show test description still not working.

--- resources/views/Backend/TeacherInterface/Modals/Tests/Activate.blade.php
+++ resources/views/Backend/TeacherInterface/Modals/Tests/Activate.blade.php
@@ -21,7 +21,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Create</button>
+                <button type="submit" class="btn btn-blue">Create</button>
             </div>
             {!! Form::close() !!}
         </div>
