9dab4ecd9e90f9d22b541a46b6bdf384484f8eaa
BusekD <dominobusek@gmail.com>
2020-02-10 12:21:15 +0100

    add search button in backend

--- resources/views/Backend/TeacherInterface/content/Tests/header.blade.php
+++ resources/views/Backend/TeacherInterface/content/Tests/header.blade.php
@@ -1,9 +1,16 @@
 <header class="content-header py-2 px-2 shadow-sm rounded border border-secondary w-100">
     <div class="forms col-sm-5 d-inline-block">
         {{ Form::search('search',null,[
-        'class'=>'form-control',
-        'placeholder'=>'Search Tests',
-        ]) }}
+        'class'=>'form-control search_everything',
+        'placeholder'=>'Search Materials',
+        ])}}
+        {{--Form::submit('Search',[
+        'class' =>'btn btn-orange',
+        'id' => 'submit_button_materials'
+        ])}}--}}
+        {{Form::submit('Search',[
+        'class' =>'btn btn-orange submit_button_everywhere',
+        ])}}
     </div>
     <button class="btn btn-blue float-lg-right mr-2" onclick="window.location='{{ route('testCreate') }}'"> New Test</button>
 </header>
