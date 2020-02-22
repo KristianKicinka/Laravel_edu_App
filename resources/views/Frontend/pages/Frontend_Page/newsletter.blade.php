4429d7b48d8eab72ee1f594958f686332c3b18bc
BusekD <dominobusek@gmail.com>
2020-02-22 14:44:44 +0100

    Deadline for tests

--- resources/views/Frontend/pages/Frontend_Page/newsletter.blade.php
+++ resources/views/Frontend/pages/Frontend_Page/newsletter.blade.php
@@ -1,7 +1,7 @@
 <div id="newsletter">
     <h3>Chceš byť stále informovaný o novinkách na portáli?</h3>
     <p>Prihlás sa na odber nášho newslettra</p>
-    {{Form::open(['method'=>'post','url'=>route('mailSender')])}}
+    {{Form::open(['method'=>'get','url'=>route('mailSender')])}}
         <input type="email" name="sendmail" placeholder="email@example.com" id="email_newsletter">
         <input type="submit" name="submit" value="Odoberať" id="submit" class="btn btn-orange">
     {{Form::close()}}
