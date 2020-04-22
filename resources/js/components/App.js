609901b895cbbea622b82177604c03fb9334befc
Kristián Kičinka <kicinka@spse-po.sk>
2020-04-22 19:09:28 +0200

    After add callFunction

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -22,7 +22,7 @@ export default class App extends Component {
        /* this.setupPusher = this.setupPusher.bind(this);*/
         this.startPeer = this.startPeer.bind(this);
     }
-    UNSAFE_componentWillMount() {
+    callFunction() {
         this.mediaHandler.getPermissions()
             .then((stream)=>{
                 this.setState({hasMedia:true});
